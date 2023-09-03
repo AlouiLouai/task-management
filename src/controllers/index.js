85959716785a6786119150605004a61b03dd39ca
AlouiLouai <louaialoui1993@gmail.com>
2023-09-03 15:35:31 +0100

    register + login working still stuck on send-verification-email

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const UserConstroller = require("./user.controller");
+const AuthConstroller = require("./auth.controller");
 
 module.exports = {
   UserConstroller,
+  AuthConstroller,
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,12 @@
 const UserService = require("./user.service");
 const EmailService = require("./email.service");
+const TokenService = require("./token.service");
+const AuthService = require("./auth.service");
+
 
 module.exports = {
   UserService,
-  EmailService
+  EmailService,
+  TokenService,
+  AuthService,
 };
